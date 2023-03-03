47d109da18d8b696e86c135f48295635844a36fc
José Marcos Lopes Damasceno <jose.damasceno@keyrus.com.br>
2023-03-02 19:36:04 -0500

    feat: add get, patch and delete routes

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -8,3 +8,39 @@ export async function createProduct (req, res) {
     res.status(500).json(err)
   }
 }
+
+export async function getProducts (req, res) {
+  try {
+    const result = await Product.find()
+    res.status(200).json(result)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
+
+export async function getProduct (req, res) {
+  try {
+    const result = await Product.findById(req.params.id)
+    res.status(200).json(result)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
+
+export async function updateProduct (req, res) {
+  try {
+    const result = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.status(200).json(result)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
+
+export async function deleteProduct (req, res) {
+  try {
+    const result = await Product.findByIdAndDelete(req.params.id)
+    res.status(200).json(result)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -3,5 +3,9 @@ import * as control from '../controllers/product.controller.js'
 const router = express.Router()
 
 router.post('/products', control.createProduct)
+router.get('/products', control.getProducts)
+router.get('/products/:id', control.getProduct)
+router.patch('/products/:id', control.updateProduct)
+router.delete('/products/:id', control.deleteProduct)
 
 export default router
